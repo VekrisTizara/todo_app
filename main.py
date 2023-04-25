24925297e668bbd9134877ed73686a65335bdc72
VekrisTizara <94655538+VekrisTizara@users.noreply.github.com>
2023-04-25 20:43:14 +0200

    make connection with mongo db store credentials in configs.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from pymongo.mongo_client import MongoClient
+from configs import db_login, db_password
 
+uri = f"mongodb+srv://{db_login}:{db_password}@cluster0.hzsg4od.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
